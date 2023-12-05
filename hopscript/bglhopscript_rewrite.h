d43875e0123686c855f1087f3fdd372df094e322
Lugrim <Lugrim@users.noreply.github.com>
2023-12-05 11:20:36 +0100

    Added missing argument to HOP_REWRITE_INIT

--- hopscript/bglhopscript_rewrite.h
+++ hopscript/bglhopscript_rewrite.h
@@ -24,8 +24,8 @@
 
 #  define HOP_REWRITE_LOCATIONS(n) \
    static Rewrite_CE hop_rewrite_locations[n] = { NULL }
-#  define HOP_REWRITE_INIT() \
-     init_rewrite_lib();
+#  define HOP_REWRITE_INIT(n) \
+     init_rewrite_lib(n);
 #  define HOP_REWRITE_CACHE_HIT(n) \
      BINREWRITELIB_EXPAND_LABEL(n):
 #  define HOP_REWRITE_CACHE_MISS(n, obj) \
